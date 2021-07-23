51a660f4b86230c0ddde6c64ecd427d147b53bf1
Bluemarble <bluemarble@pop-os.localdomain>
2021-07-22 23:17:16 -0300

    first commit

---
+++ install.sh
@@ -0,0 +1,37 @@
+#!/bin/bash
+
+#INSTALA O GNOME TWEAKS
+sudo apt install gnome-tweaks && echo "AJUSTES INSTALADO";
+
+#INSTALA O ZSH
+sudo apt install zsh && echo "ZSH INSTALADO";
+
+#INSTALA O SNAPD
+sudo apt install snapd && echo "SNAP INSTALADO";
+
+#INSTALA O TELEGRAM
+sudo apt install telegram-desktop && echo "TELEGRAM INSTALADO";
+
+#INSTALA O CHROME
+#wget https://dl.google.com/linux/direct/google-chrome-stable_current_amd64.deb && cd Documentos/ && sudo dpkg -i google-chrome-stable_current_amd64.deb;
+
+#INSTALA O NVM
+wget -qO- https://raw.githubusercontent.com/nvm-sh/nvm/v0.38.0/install.sh | bash;
+
+export NVM_DIR="$([ -z "${XDG_CONFIG_HOME-}" ] && printf %s "${HOME}/.nvm" || printf %s "${XDG_CONFIG_HOME}/nvm")"
+[ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh"; # This loads nvm
+
+command -v nvm;
+
+nvm install 14.0.0;
+
+#INSTALA O YARN
+
+npm install -g yarn && echo "YARN INSTALADO";
+
+#INSTALA O INSOMNIA
+sudo snap install insomnia && echo "INSOMNIA INSTALADO";
+
+#INSTALA O VISUAL STUDIO CODE
+sudo apt install code && echo "Visual studio code instalado";
+
