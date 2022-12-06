3f3eba4fa4a8c80396c0329ce59855b74a53d59e
Gabriel <gabriel@pop-os.localdomain>
2022-12-06 12:08:46 -0300

    feat: config fish shell

--- install.sh
+++ install.sh
@@ -15,8 +15,25 @@ sudo dpkg -i fish_3.5.1-1~xenial_amd64.deb && rm fish_3.5.1-1~xenial_amd64.deb &
 # Definiciar o fish como shell padrão
 chsh -s /bin/fish
 
+# Configurar o fish com startship
+curl -sS https://starship.rs/install.sh | sh
+
+echo "starship init fish | source" >> ~/.config/fish/config.fish
+
+cp ./starship.toml ~/.config/starship.toml
+
+echo "Startship configurado";
 
 #INSTALA o nvm
+curl -sL https://git.io/fisher | source && fisher install jorgebucaran/fisher
+
+fisher install jorgebucaran/nvm.fish
+
+nvm install lts && nvm use lts && echo "NVM INSTALADO";
+
+echo "nvm use lts" >> ~/.config/fish/config.fish;
+
+echo "NVM configurado";
 
 #INSTALA o visual studio code
 
