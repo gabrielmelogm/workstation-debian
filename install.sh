d2865c126b644dead55d0bd63a0196cd347cc7ff
Gabriel <gabriel@pop-os.localdomain>
2022-12-06 12:34:24 -0300

    feat: instala pacotes flatpak

--- install.sh
+++ install.sh
@@ -43,12 +43,20 @@ echo "starship init fish | source" \n "nvm use lts" >> ~/.config/fish/config.fis
 echo "Fish configurado"
 
 #INSTALA o visual studio code
+wget -c https://az764295.vo.msecnd.net/stable/6261075646f055b99068d3688932416f2346dd3b/code_1.73.1-1667967334_amd64.deb
+sudo dpkg -i code_1.73.1-1667967334_amd64.deb && rm code_1.73.1-1667967334_amd64.deb && echo "VS CODE INSTALADO";
 
 #INSTALA o telegram
+flatpak install flathub org.telegram.desktop && echo "Telegram instalado";
 
 #INSTALA o insomnia
+flatpak install flathub rest.insomnia.Insomnia && echo "Insomnia instalado";
 
 #INSTALA o spotify
 flatpak install flathub com.spotify.Client && echo "Spotify instalado";
 
 # INSTALA o slack
+flatpak install flathub com.slack.Slack && echo "Slack instalado";
+
+# INSTALA o discord
+flatpak install flathub com.discordapp.Discord && echo "Discord instalado";
