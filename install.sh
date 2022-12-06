09555e8279296f9dc47ab1338c6f848e286ac811
Gabriel <gabriel@pop-os.localdomain>
2022-12-06 12:29:33 -0300

    feat: instala as fontes

--- install.sh
+++ install.sh
@@ -3,6 +3,13 @@
 #INSTALA O GNOME TWEAKS
 sudo apt install gnome-tweaks && echo "AJUSTES INSTALADO";
 
+#INSTALA as fontes ligatures
+mkdir ~/.fonts
+
+cp ./FiraCode.zip ~/.fonts/FiraCode.zip && unzip ~/.fonts/FiraCode.zip && rm ~/.fonts/FiraCode.zip && echo "FONTE INSTALADA";
+
+cp ./JetBrainsMono-2.242.zip ~/.fonts/JetBrainsMono-2.242.zip && unzip ~/.fonts/JetBrainsMono-2.242.zip && rm ~/.fonts/JetBrainsMono-2.242.zip && echo "FONTE INSTALADA";
+
 #INSTALA o wget (caso não tenha)
 sudo apt install wget && echo "WGET INSTALADO";
 
@@ -13,7 +20,7 @@ wget -c https://launchpadlibrarian.net/613913678/fish_3.5.1-1~xenial_amd64.deb
 sudo dpkg -i fish_3.5.1-1~xenial_amd64.deb && rm fish_3.5.1-1~xenial_amd64.deb && echo "FISH INSTALADO";
 
 # Definiciar o fish como shell padrão
-chsh -s /bin/fish
+chsh -s /usr/bin/fish
 
 # Configurar o fish com startship
 curl -sS https://starship.rs/install.sh | sh
